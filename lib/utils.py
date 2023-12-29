8076029df823c14f8200f1ac8b4d927a0a461ac5
Veronica Valeros <vero.valeros@gmail.com>
2023-12-29 17:46:06 +0100

    Add function to store set to disk

---
+++ lib/utils.py
@@ -0,0 +1,18 @@
+"""
+Set of common util functions to supopr the
+functionality of the web crawler
+"""
+
+def store_set_to_file(set_to_save_to_disk, output_dir, file_name):
+    """
+    Writes the contents of a set to a file, with each element on a new line.
+
+    :param set_to_save_to_disk: The set whose contents need to be written to the file.
+    :param output_dir: The directory to store the files.
+    :param file_name: The name of the file to write to.
+    """
+
+    output_filename = f"{output_directory}/{file_name}.log"
+    with open(output_filename, "w") as file:
+        for item in set_to_save_to_disk:
+            file.write(item + "\n")
