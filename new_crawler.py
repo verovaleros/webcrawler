f0b13c9c679e2c567219720115b27d4460928007
Veronica Valeros <vero.valeros@gmail.com>
2023-12-29 16:28:43 +0100

    Add basic logic to process the queues

--- new_crawler.py
+++ new_crawler.py
@@ -10,6 +10,7 @@ import argparse
 import os
 import re
 import logging
+import requests
 from collections import deque
 from lib.fetch_website import fetch_website
 
@@ -24,12 +25,12 @@ def create_parser():
     parser.add_argument('-D', '--debug', action='store_true', help='Debug')
     parser.add_argument('-u', '--url', required=True, type=str, help='URL to start crawling')
     parser.add_argument('-w', '--write', action='store_true', help='Save crawl output to a local file')
-    parser.add_argument('-L', '--common-log-format', action='store_true', help='Generate log of the requests in CLF')
-    parser.add_argument('-e', '--export-file-list', action='store_true', help='Creates a file with all the URLs to found files during crawling')
-    parser.add_argument('-l', '--crawl-limit', type=int, help='Maximum links to crawl')
-    parser.add_argument('-C', '--crawl-depth', type=int, help='Limit the crawling depth according to the value specified')
-    parser.add_argument('-d', '--download-file', type=str, help='Specify the file type of the files to download')
-    parser.add_argument('-i', '--interactive-download', action='store_true', help='Before downloading files allow user to specify manually the type of files to download')
+    parser.add_argument('-L', '--common-log-format', default=False, action='store_true', help='Generate log of the requests in CLF')
+    parser.add_argument('-e', '--export-file-list', default=False, action='store_true', help='Creates a file with all the URLs to found files during crawling')
+    parser.add_argument('-l', '--crawl-limit', type=int, default=float('inf'), help='Maximum links to crawl')
+    parser.add_argument('-C', '--crawl-depth', type=int, default=float('inf'), help='Limit the crawling depth according to the value specified')
+    parser.add_argument('-d', '--download-file', type=str, default=False, help='Specify the file type of the files to download')
+    parser.add_argument('-i', '--interactive-download', default=False, action='store_true', help='Before downloading files allow user to specify manually the type of files to download')
     parser.add_argument('-U', '--username', type=str, help='User name for authentication')
     parser.add_argument('-P', '--password', type=str, help='Request password for authentication')
     return parser
@@ -73,21 +74,43 @@ def main():
     args = parser.parse_args()
 
     urls_parsed = set()
-    urls_queued = set()
+    urls_queued = deque()
     urls_failed = set()
 
     setup_logging(args.verbose, args.debug, args.url)
     logging.debug('Debug mode is on')
 
     # Process the root URL
-    root_url = args.url
-
-    urls_parsed.add(root_url)
-
-    result = fetch_website(root_url, args.username, args.password)
-    logging.info('Crawled: %s - %s', root_url, result['status_code'])
+    urls_queued.append(args.url)
+    logging.info('Web crawling starting on base URL %s', args.url)
 
 
+    # Limit the URLs processed according to the input limit
+    while len(urls_parsed) <= args.crawl_limit and len(urls_queued) > 0:
+        with requests.Session() as session:
+
+            current_url = urls_queued.popleft()
+
+            response = fetch_website(session, current_url, args.username, args.password)
+
+            logging.info('CRAWLED - %s - %s - %.2f Kb', current_url, response.status_code, len(response.content)/1024)
+
+            # Parse the response status codes
+            if response.ok:
+                urls_parsed.add(current_url)
+                if response.headers.get('Location', None) is not None:
+                    urls_queued.append(response.headers.get('Location', None))
+            else:
+                urls_failed.add(current_url)
+
+            # Parse the response content
+
+    # Log summary of the results
+    logging.info('SUMMARY - Crawled: %i, Queued: %i, Failed: %i ',
+                 len(urls_parsed),
+                 len(urls_queued),
+                 len(urls_failed)
+                 )
 
 if __name__ == "__main__":
     main()
